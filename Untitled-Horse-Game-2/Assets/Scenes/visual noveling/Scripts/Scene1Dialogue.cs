9567d2b3aadddfacfc3cdd9de28401efb7f63f77
ecKLsCakes <kaylee08@comcast.net>
2026-04-22 09:41:45 -0400

    very scuffed very elementary visual novel scene added

---
+++ Untitled-Horse-Game-2/Assets/Scenes/visual noveling/Scripts/Scene1Dialogue.cs
@@ -0,0 +1,148 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class Scene1Dialogue : MonoBehaviour {
+        public int primeInt = 1;
+        public TMP_Text Char1name;
+        public TMP_Text Char1speech;
+        public TMP_Text Char2name;
+        public TMP_Text Char2speech;
+        public GameObject DialogueDisplay;
+        public GameObject ArtBG1;
+        public GameObject Choice1a;
+        public GameObject Choice1b;
+        public GameObject NextScene1Button;
+        public GameObject NextScene2Button;
+        public GameObject nextButton;
+       //public AudioSource audioSource1;
+        private bool allowSpace = true;
+
+// Set initial visibility. Added images or buttons need to also be SetActive(false);
+        void Start(){  
+             DialogueDisplay.SetActive(false);
+             ArtBG1.SetActive(true);
+             Choice1a.SetActive(false);
+             Choice1b.SetActive(false);
+             NextScene1Button.SetActive(false);
+             NextScene2Button.SetActive(false);
+             nextButton.SetActive(true);
+        }
+
+// Use the spacebar as a faster "Next" button:
+        void Update(){        
+             if (allowSpace == true){
+                 if (Input.GetKeyDown("space")){
+                      Next();
+                 }
+
+                 // secret debug code: go back 1 Story Unit, if NEXT is visible
+                 if (Input.GetKeyDown("p")) {
+                      primeInt -= 2;
+                      Next();
+                 }
+             }
+        }
+
+//Story Units! The main story function.
+//Players hit [NEXT] to progress to the next primeInt:
+public void Next(){
+        primeInt += 1;
+        if (primeInt == 1){
+                // audioSource1.Play();
+        }
+        else if (primeInt == 2){
+                DialogueDisplay.SetActive(true);
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "god????";
+                Char2speech.text = "hey,,,, you look like you need this";
+        }
+       else if (primeInt ==3){
+                Char1name.text = "horse";
+                Char1speech.text = "my god";
+                Char2name.text = "";
+                Char2speech.text = "";
+                //gameHandler.AddPlayerStat(1);
+        }
+       else if (primeInt == 4){
+                Char1name.text = "horse";
+                Char1speech.text = "it's a 1872 carolina whiskey";
+                Char2name.text = "";
+                Char2speech.text = "";
+                // Turn off the "Next" button, turn on "Choice" buttons
+                nextButton.SetActive(false);
+                allowSpace = false;
+                Choice1a.SetActive(true); // function Choice1aFunct()
+                Choice1b.SetActive(true); // function Choice1bFunct()
+        }
+
+       // after choice 1a
+       else if (primeInt == 20){
+                //gameHandler.AddPlayerStat(1);
+                Char1name.text = "";
+                Char1speech.text = "it tastes like heaven";
+                Char2name.text = "";
+                Char2speech.text = "";
+                // Turn off the "Next" button, turn on "Scene" button/s
+                nextButton.SetActive(false);
+                allowSpace = false;
+                NextScene1Button.SetActive(true);
+        }
+
+       // after choice 1b
+       else if (primeInt == 30){
+                Char1name.text = "";
+                Char1speech.text = "you get up again and sleep standing";
+                Char2name.text = "";
+                Char2speech.text = "";
+        }
+       else if (primeInt == 31){
+                Char1name.text = "";
+                Char1speech.text = "as you are a horse";
+                Char2name.text = "";
+                Char2speech.text = "";
+                // Turn off the "Next" button, turn on "Scene" button/s
+                nextButton.SetActive(false);
+                allowSpace = false;
+                NextScene2Button.SetActive(true);
+        }
+
+      //Please do NOT delete this final bracket that ends the Next() function:
+     }
+
+// FUNCTIONS FOR BUTTONS TO ACCESS (Choice #1 and SceneChanges)
+        public void Choice1aFunct(){
+                Char1name.text = "";
+                Char1speech.text = "it tastes like heaven";
+                Char2name.text = "";
+                Char2speech.text = "";
+                primeInt = 19; // so hitting "NEXT" goes to primeInt==20!
+                Choice1a.SetActive(false);
+                Choice1b.SetActive(false);
+                nextButton.SetActive(true);
+                allowSpace = true;
+        }
+        public void Choice1bFunct(){
+                Char1name.text = "";
+                Char1speech.text = "you lay down on the rickety bed";
+                Char2name.text = "";
+                Char2speech.text = "";
+                primeInt = 29; // so hitting "NEXT" goes to primeInt==30!
+                Choice1a.SetActive(false);
+                Choice1b.SetActive(false);
+                nextButton.SetActive(true);
+                allowSpace = true;
+        }
+
+        public void SceneChange1(){
+               SceneManager.LoadScene("Scene2a");
+        }
+        public void SceneChange2(){
+                SceneManager.LoadScene("Scene2b");
+        }
+}
